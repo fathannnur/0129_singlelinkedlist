77a8c43b74d3c817b59c2326b92f08e01aa70735
fathannnur <carelius02@gmail.com>
2026-04-23 11:15:37 +0700

    Menambahkan return

--- Single.LinkedList.cpp
+++ Single.LinkedList.cpp
@@ -22,3 +22,21 @@ public:
     void addNode()
     {
         int nim;
+        cout << "\nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            { 
+                cout << "\nDuplikasi noMhs tidak diizinkan\n";
+                return;
+            }
+
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
